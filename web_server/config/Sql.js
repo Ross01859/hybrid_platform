b8bfaf6c31feb3ff4a7e398840cfdd8397cd19d5
wangruishan <wangruishan@chaomengdata.com>
2018-05-08 17:22:23 +0800

    删除client

--- web_server/config/Sql.js
+++ web_server/config/Sql.js
@@ -1,7 +1,7 @@
 module.exports = {
     usersql: {
         login: 'SELECT * FROM user WHERE username = ? AND password=?',
-        insert: 'INSERT INTO user(username,sex,age,tel) VALUES(?,?,?,?)',
+        insert: 'INSERT INTO user(username,password,sex,age,tel) VALUES(?,?,?,?,?)',
         queryAll: 'SELECT * FROM user',
         getUserByName: 'SELECT * FROM user WHERE username = ? ',
     },
--- web_server/routes/index.js
+++ web_server/routes/index.js
@@ -1,9 +1,29 @@
-var express = require('express')
+var express = require( 'express' )
 var router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index')
-})
+router.get( '/', function( req, res, next ) {
+    res.render( 'index' )
+} )
 
+router.post( '/add/user', function( req, res, next ) {
+    console.log( '+++++++++++++++++++++++++++++++++++' )
+    var saveUser = {
+        username: req.body.name,
+        password: req.body.password,
+        age: req.body.age,
+        sex: req.body.sex,
+        tel: req.body.weight
+    }
+    return db.sequelize.transaction( function( t ) {
+        console.log( '+++++++++++++++++++++++++++' )
+        return User.create( saveUser, {
+            transaction: t
+        } ).then( function( result ) {
+            res.send( result )
+        } ).catch( function( err ) {
+            console.log( '发生错误：' + err )
+        } )
+    } )
+} )
 module.exports = router
