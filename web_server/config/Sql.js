a4394fe413ff3f3e2bcd74ade2c4884f88685d87
lrmuyi <1342940006@qq.com>
2018-05-12 00:30:41 +0800

    Revert "Bdev"

---
+++ web_server/config/Sql.js
@@ -0,0 +1,13 @@
+module.exports = {
+    usersql: {
+        login: 'SELECT * FROM user WHERE username = ? AND password=?',
+        insert: 'INSERT INTO user(username,sex,age,tel) VALUES(?,?,?,?)',
+        queryAll: 'SELECT * FROM user',
+        getUserByName: 'SELECT * FROM user WHERE username = ? ',
+    },
+    goodlist: {
+        insert: 'INSERT INTO goods(name,price,number,des) VALUES(?,?,?,?)',
+        queryAll: 'SELECT * FROM goods',
+        getUserByName: 'SELECT * FROM goods WHERE name = ? ',
+    }
+}
--- web_server/routes/index.js
+++ web_server/routes/index.js
@@ -1,35 +1,9 @@
-const express = require( 'express' );
-const jwt = require( "jsonwebtoken" );
-const config = require( '../config' );
-// 建立 Router 物件
-const router = express.Router();
+var express = require('express')
+var router = express.Router()
 
-// 在每一个请求被处理之前都会执行的middleware
-router.use( function( req, res, next ) {
-    req.session.secret = config.sequelize.secret;
-    if ( req.url != '/login' && req.url != '/register' ) {
-        //&& req.url != '/goods' && req.url != '/bugs_sea'
-        let token = req.query.token || req.params.token || req.headers[ 'x-access-token' ];
-        if ( !token ) {
-            jwt.verify( token, req.session.secret, function( err, decoded ) {
-                if ( !err ) {
-                    return res.json( { error_code: 3, result: 'Failed to authenticate token.' } )
-                } else {
-                    next();
-                }
-            } )
-        } else {
-            return res.json( { error_code: 3, result: 'No token provided.' } )
-        }
-    } else {
-        // 输出记录信息
-        // console.log( req.method, req.url );
-        // 继续路由处理
-        next();
-    }
-} );
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index')
+})
 
-const userRouter = require( './user' )( router );
-const goodsRouter = require( './goods' )( router );
-const bugsRouter = require( './bugs' )( router );
-module.exports = router;
+module.exports = router
